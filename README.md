733d6e184dab47285e9ec9e94832291a9a255296
Finn Upham <finn.upham@gmail.com>
2024-09-27 16:18:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+README.md
+
+Code to prepare and analyse audience motion measurements from chest mounted accelerometers (multiple device types) during a chamber music performance. Results for paper and supplementary materials. Unless specified otherwise, all supporting data available on OSF repo for MusicLab Copenhagen (2021). 
+
+2024 Finn Upham, Fernando Rosas
---
+++ am.py
@@ -0,0 +1,138 @@
+# list of definitions for processing audience chest-mounted ACC measurements
+# Upham 2024, from CopenML_Analysis (2022)
+
+import sys
+import os
+import time
+import datetime as dt
+import math
+import numpy as np 
+import scipy as sp
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.signal import butter,filtfilt
+from scipy import interpolate
+from scipy.interpolate import interp1d
+
+def tic():
+    ticked  = time.time()
+    return ticked
+
+def toc(ticked):
+    print(' '.join(['\nTime passed',str(np.round(time.time()-ticked,2)),'s']))
+    return
+
+# NEW NO PD APPEND, 
+# NEW empty no longer treated as np.nan assigned by default
+def gap_nans(data,gap_t):
+    # data is a pandas dataframe with columns called time and/or time_concert which is used to ID gaps greater than gap_T
+    # gap_T is in milliseconds (ms)
+    # rows of NaN data is added to non-time columns before the first sample, at edges of each gap, and after the last sample of data
+    # these nans prevent interpolation errors through missing data
+    cols = data.columns
+    if 'time' in cols:
+        time_col = 'time'
+    if 'time_concert' in cols:
+        time_col = 'time_concert'
+        
+    deltat = round(0.35*data[time_col].diff().median())
+    dtdeltat =  pd.to_timedelta(deltat,unit = 'ms')
+    dt = data[time_col].diff()    
+    gapsi = np.array(dt[dt>gap_t].index)
+    a = np.append(gapsi-1,dt.index[-1])
+    gapsd = pd.DataFrame(index = a,columns = data.columns)
+    for c in cols:
+        if c.startswith('datetime'):
+            gapsd[c] = data.loc[a,c]+dtdeltat
+        else:
+            if c.lower().startswith('time'):
+                gapsd[c] = data.loc[a,c]-deltat
+            else:
+                gapsd[c] = np.nan
+    a = np.append(gapsi,dt.index[0])
+    gapsp = pd.DataFrame(index = a,columns = data.columns)
+    for c in cols:
+        if c.lower().startswith('datetime'):
+            gapsp[c] = data.loc[a,c]-dtdeltat
+        else:
+            if c.lower().startswith('time'):
+                gapsp[c] = data.loc[a,c]-deltat
+            else:
+                gapsp[c] = np.nan
+    data = pd.concat([df for df in [gapsp,gapsd,data] if not df.empty]).sort_values(time_col,ignore_index=True)
+    return data
+
+def butter_lowpass_filter(data, cutoff, fs, order):
+    normal_cutoff = cutoff / (fs/2)
+    # Get the filter coefficients 
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    y = filtfilt(b, a, data)
+    return y
+
+# https://stackoverflow.com/questions/11517986/indicating-the-statistically-significant-difference-in-bar-graph
+def barplot_annotate_brackets(num1, num2, data, center, height, yerr=None, dh=.05, barh=.05, fs=None, maxasterix=None):
+    """ 
+    Annotate barplot with p-values.
+
+    :param num1: number of left bar to put bracket over
+    :param num2: number of right bar to put bracket over
+    :param data: string to write or number for generating asterixes
+    :param center: centers of all bars (like plt.bar() input)
+    :param height: heights of all bars (like plt.bar() input)
+    :param yerr: yerrs of all bars (like plt.bar() input)
+    :param dh: height offset over bar / bar + yerr in axes coordinates (0 to 1)
+    :param barh: bar height in axes coordinates (0 to 1)
+    :param fs: font size
+    :param maxasterix: maximum number of asterixes to write (for very small p-values)
+    """
+
+    if type(data) is str:
+        text = data
+    else:
+        # * is p < 0.05
+        # ** is p < 0.005
+        # *** is p < 0.0005
+        # etc.
+        text = ''
+        p = .05
+
+        while data < p:
+            text += '*'
+            p /= 10.
+
+            if maxasterix and len(text) == maxasterix:
+                break
+
+        if len(text) == 0:
+            text = 'n. s.'
+
+    lx, ly = center[num1], height[num1]
+    rx, ry = center[num2], height[num2]
+
+    if yerr:
+        ly += yerr[num1]
+        ry += yerr[num2]
+
+    ax_y0, ax_y1 = plt.gca().get_ylim()
+    dh *= (ax_y1 - ax_y0)
+    barh *= (ax_y1 - ax_y0)
+
+    y = max(ly, ry) + dh
+
+    barx = [lx, lx, rx, rx]
+    bary = [y, y+barh, y+barh, y]
+    mid = ((lx+rx)/2, y+barh)
+
+    plt.plot(barx, bary, c='black')
+
+    kwargs = dict(ha='center', va='bottom')
+    if fs is not None:
+        kwargs['fontsize'] = fs
+
+    plt.text(*mid, text, **kwargs)
+    
+def sampVar(S):
+    s2 = (S-S.mean()).pow(2).sum()/(len(S)-1)
+          
+    return s2
